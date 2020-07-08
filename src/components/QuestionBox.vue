60344a6c3922e4b1b708909bd7587b61fbd9789b
ingridwy <bluebear589@gmail.com>
2020-07-08 17:36:13 +0100

    answers shuffle by importing lodash library

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -8,10 +8,13 @@
         </template>
 
         <hr class="my-4">
-            <b-list-group v-for="(answer,index) in answers" :key="index" @click="select(index)">
-                <b-list-group-item>{{ answer }}</b-list-group-item>
+            <b-list-group >
+                <b-list-group-item v-for="(answer,index) in answers" :key="index" @click="select(index)"
+                :class="[selectIndex === index?  'selected' : '']">
+                    {{ answer }}
+                </b-list-group-item>
             </b-list-group>
-
+ 
         <b-button variant="primary" href="#">Submit</b-button>
         <b-button @click="next" variant="success" href="#">Next Q</b-button>
     </b-jumbotron>
@@ -19,6 +22,7 @@
 
 </template>
 <script>
+    import _ from 'lodash'
     export default { 
         props: {
             theQuestion: Object,
@@ -26,24 +30,35 @@
         },
         data() {
             return{
-                selectIndex: null
+                selectIndex: null,
+                shuffledAnswer: []
                 } 
+        },  
+        computed:{
+            answers(){
+                let answers=[...this.theQuestion.incorrect_answers]
+                answers.push(this.theQuestion.correct_answer) 
+                return answers
+            }
+        },
+        watch:{
+            theQuestion(){
+                this.selectIndex = null
+                this.shuffleAnswer()
+            }
         },
         methods:{
             select(index) {
                 this.selectIndex = index
+                //console.log(index)
+            },
+            shuffleAnswer(){
+                let answers = [...this.theQuestion.incorrect_answers , this.theQuestion.correct_answer]
+                this.shuffledAnswer = _.shuffle(answers)
             }
-        }, 
-        computed:{
-            answers(){
-                let answers=[...this.theQuestion.incorrect_answers]
-                answers.push(this.theQuestion.correct_answer)
-                return answers
-            }
-        }, 
-        mounted() {
-        console.log(this.thisQuestion)
-    }
+
+        }
+      
     }
     
 </script>
@@ -58,7 +73,7 @@
     margin: 5px;
 }
 .selected{
-    background-color: #E5E5E5;
+    background-color: #FFB6C1;
 }
 .right{
     background-color: green;
